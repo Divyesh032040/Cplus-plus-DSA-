3d085aae5ab4b119ed89413b2b36cf7cc94881e3
Divyesh Parmar <127118175+Divyesh032040@users.noreply.github.com>
2023-05-18 13:30:06 +0530

    Add files via upload

---
+++ array/palindrome - char array.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int n;
+cin>>n;
+
+char arr[n+1];
+cin>>arr;
+
+bool check = 1;
+
+for (int i = 0; i < n ; i++)
+{
+    if (arr[i]!=arr[n-1-i])
+    {
+        check = 0 ;
+        break;
+    }
+}
+
+if (check== true)
+{
+    cout<<"word id palindrome"<<endl;
+}
+else {
+    cout<<"word is not palindrome"<<endl;
+}
+
+    return 0;
+}
