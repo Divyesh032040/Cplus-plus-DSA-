3bfff5d3b0d61ed2a850e4f355cea600b023fbf6
Soham Piprotar <123930437+Soham1305@users.noreply.github.com>
2023-05-29 15:42:16 +0530

    Update binary searching in array c++.cpp
    It's is better to use Divide and concours algorithm , and it's also has time complexity of O(logn) . I suggest you to adapt this code because this algorithm can work in highly big data with most efficiency

--- searching in array/binary searching in array c++.cpp
+++ searching in array/binary searching in array c++.cpp
@@ -1,39 +1,80 @@
-#include<iostream>
+// Binary search usig recurcive method ;
+
+// Time complexity = O(log(N));
+
+// code by soham piprotar
+#include <iostream>
+#include <vector>
+#include <stack>    
+#include <queue>
+#include <cmath>
+#include <climits>
+#include <algorithm>
+#include <string> 
+#include <map>
+#include <set> 
+//#include <bits/stdc++.h>
+
+#define vi vector<int>
+#define vvi vector <vi> 
+#define pii pair<int,int>
+#define vii vector<pii>
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define ff first 
+#define ss second
+#define setbits(x) builtin_popcount(x)
+
 using namespace std;
+const int N = 1e5+7 ; 
 
-int binarysearch (int array[],int n,int key) {
+ int n;
 
-int s=0;
-int e=n;
-while (s<=e)
-{
-    int mid=(s+e)/2;
-    if (array[mid]==key) {
-        return mid;
+int B(int a[],int s,int e,int key){
+
+    if( s == e){
+        if(a[0] == key){
+            return s;
+        }else{
+            return -1 ;
+        }
+    }else{
+
+        int mid = (s+e)/2;
+
+        if(a[mid] == key){
+            return mid ;
+        }if( a[mid] > key){
+            return B(a,s,mid-1,key);
+        }else{
+            return B(a,mid+1,e,key);
+        }
     }
-    else if(array[mid]>key) {
-        e=mid-1;
+
+    return -1 ;
+}
+
+int main(){
+
+    int n;
+    cin>>n;
+
+    int a[n];
+
+    int key ;cin>>key ;
+
+    for(int i=0 ;i<n;i++){
+
+        cin>>a[i];
     }
-    else {
-        s=mid+1;
+
+    int d =B(a,0,n-1,key);
+
+    if( d != -1){
+        cout<<" Element Found in array at : "<<d<<endl;
+    
+    }else{
+        cout<<" Element is not found in array :"<<endl;
     }
-}
-return -1;
-}
 
-int main ()  {
-
-int n;
-cin>>n;
-int array [n];
-for (int i = 0; i < n; i++)
-{
-    cin>>array[i];
-}
-int key;
-cin>>key;
-
-cout<<binarysearch (array,n,key)<<endl;
-
-    return 0;
+    return 0 ;
 }
