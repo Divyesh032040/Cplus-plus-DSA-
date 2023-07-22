a33e7eece4c3dbe86e2b3bac806e53a5db5784bf
Divyesh Parmar <127118175+Divyesh032040@users.noreply.github.com>
2023-07-22 11:13:41 +0530

    Update Q.3 arr is sorted or not.cpp

--- recursion/Q.3 arr is sorted or not.cpp
+++ recursion/Q.3 arr is sorted or not.cpp
@@ -1,13 +1,13 @@
 #include<iostream>
 using namespace std;
 
-int sorted( int arr[] , int n){
+bool sorted( int arr[] , int n){
 
     if(n==1){
         return true;
     }
 
-    int restarry = sorted( arr+1 , n-1 );
+    bool restarry = sorted( arr+1 , n-1 );
     return ( arr[0] < arr[1] && restarry );
 }
 int main(){
