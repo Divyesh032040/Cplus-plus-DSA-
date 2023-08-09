53dd123a08c1925365a9b9c04dc0c5aac87b098f
Divyesh Parmar <127118175+Divyesh032040@users.noreply.github.com>
2023-08-09 08:43:27 +0530

    Update Vector Pair question.cpp
    changing input value and write output

--- STL/Vector Pair question.cpp
+++ STL/Vector Pair question.cpp
@@ -17,7 +17,7 @@ bool myCompare(pair<int, int> p1, pair<int, int> p2) {
 
 int main() {
 
-  int arr[] = {10, 16, 7, 14, 5, 3, 2, 9};
+  int arr[] = {10, 16, 7, 14, 5, 3, 12, 9};
   vector < pair<int, int> > v;
 
   for (int i = 0; i<(sizeof(arr) / sizeof(arr[0])); i++) {
@@ -35,3 +35,4 @@ int main() {
 
   return 0;
 }
+//output : 4 7 2 6 1 0 5 3 
