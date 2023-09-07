38d53ce60c1f6d768c6a4c1cc787d27cb480f446
Divyesh Parmar <127118175+Divyesh032040@users.noreply.github.com>
2023-09-08 02:23:59 +0530

    Add files via upload
    code to insert a nodes at tail of linked list .

---
+++ Linked List/Insert At Tail.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+
+class node{
+
+public:
+int data;
+node *next;
+
+//constructor 
+
+node(int data){
+    this -> data = data;
+    this -> next = NULL;
+}
+};
+
+// void insertAtHead( node* &head , int data ){
+//     //create a new node
+//     node* temp = new node (data);
+//     temp -> next = head ;
+//     head = temp ;
+
+// }
+
+void insertAtTail ( node* &tail , int data){
+    //creating a new node 
+    node*temp = new node(data);
+    tail -> next =  temp ; 
+    tail = temp;
+}
+ 
+void print(node* &head) {
+    node* temp = head; 
+    
+    while (temp != NULL) { 
+        cout << temp->data << " "; 
+        temp = temp->next; 
+    }
+    
+    cout << endl; 
+}
+
+int main() {
+
+//create a first new node 
+node* node1 = new node(10);
+node* head = node1;
+node* tail = node1;
+print(head);
+
+insertAtTail(tail ,20);
+print(head);
+
+insertAtTail(tail ,30);
+print(head);
+
+insertAtTail(tail ,40);
+print(head);
+
+// insertAtHead(head ,20); 
+// print(head);
+// insertAtHead(head ,30);
+// print(head);
+
+return 0;
+}
+
+//   output : 10 
+//            10 20 
+//            10 20 30 
+//            10 20 30 40 
